615df2f776471411ce451784feb8cc604c940cbe
ishevcov <shevtsow.ivan@yandex.ru>
2021-05-19 10:47:31 +0300

    19.05.21

--- Two_port_calibration_TRL.py
+++ Two_port_calibration_TRL.py
@@ -11,13 +11,14 @@ plt.rcParams["font.size"] = "14"
 Home_0 = 'SP/SOLT'
 Home_1 = 'SP/SOLT/measurelay_1'
 Home_2 = 'SP/TRL'
+N_DUT = 'DUT2'
 
 D_reflect = f'{Home_1}/open.s1p'
 D_thru    = f'{Home_1}/thru.s2p'
 D_Line_1 =  f'{Home_2}/Line_1.s2p'
-D_Line_2 =  f'{Home_2}/Line_2.s2p'
-D_DUT_Ideal = f'{Home_0}/ideal/DUT1.s2p'
-D_DUT_Measure = f'{Home_1}/DUT1.s2p'
+#D_Line_2 =  f'{Home_2}/Line_2.s2p'
+D_DUT_Ideal = f'{Home_0}/ideal/{N_DUT}.s2p'
+D_DUT_Measure = f'{Home_1}/{N_DUT}.s2p'
 
 reflect = rf.Network(D_reflect)
 
@@ -26,7 +27,7 @@ reflect = rf.Network(D_reflect)
 T = rf.Network(D_thru)
 R = rf.two_port_reflect(reflect, reflect)
 L1 = rf.Network(D_Line_1)
-L2 = rf.Network(D_Line_2)
+#L2 = rf.Network(D_Line_2)
 
 measured = [T, R, L1]
 
@@ -42,7 +43,6 @@ DUT_IDEAL = rf.Network(D_DUT_Ideal)
 
 dut_raw = rf.Network(D_DUT_Measure)
 dut_corrected = cal.apply_cal(dut_raw)
-
 
 
 plt.figure()
@@ -66,6 +66,11 @@ plt.xlabel('F, Гц')
 plt.ylabel('S21, дБ')
 plt.grid()
 
-
+Error_21_db = 20*np.log10(dut_corrected.s[1:, 1, 0])-20*np.log10(DUT_IDEAL.s[1:, 1, 0])
+F_Error = DUT_IDEAL.f[1:]
+plt.figure()
+plt.plot(F_Error, Error_21_db,    label='Ошибка', linewidth='3')
+plt.legend()
+plt.grid()
 
 plt.show()
